0b021d6f9438edf9cd42909dfc30ae46cc6222d4
mac <mac@MacBook-Pro-mac.local>
2026-04-06 14:02:51 +0300

    Add blank white front page

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Blank front page placeholder.
+ *
+ * @package graffit
+ */
+
+declare(strict_types=1);
+
+get_header();
+?>
+<main id="primary" class="site-main site-main--blank-front" aria-label="<?php esc_attr_e('Front page', 'graffit'); ?>"></main>
+<?php
+echo '</div>';
+wp_footer();
+?>
+</body>
+</html>
