fe368586f135f484ff1d488bb80d6682b585a616
mac <mac@MacBook-Pro-mac.local>
2026-04-06 17:51:53 +0300

    Add structured site footer layout

--- footer.php
+++ footer.php
@@ -4,18 +4,11 @@
  *
  * @package graffit
  */
+
+declare(strict_types=1);
 ?>
-<footer class="site-footer" id="site-footer">
-    <div class="site-footer__container">
-        <div class="site-footer__line" aria-hidden="true"></div>
-        <div class="site-footer__inner">
-            <p class="site-footer__brand">GraffiT</p>
-            <p class="site-footer__text">Програмні рішення під задачі вашого бізнесу.</p>
-        </div>
-    </div>
-</footer>
+<?php get_template_part('template-parts/layout/site', 'footer'); ?>
 </div>
 <?php wp_footer(); ?>
 </body>
 </html>
-
