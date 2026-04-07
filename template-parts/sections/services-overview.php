95e4d6a056cf27d9e49d177c66c2e4c0e64c54a1
mac <mac@MacBook-Pro-mac.local>
2026-04-07 14:16:14 +0300

    remove default active service card

--- template-parts/sections/services-overview.php
+++ template-parts/sections/services-overview.php
@@ -21,7 +21,7 @@ $cards = [
         'image_position' => 'center',
         'button_label' => 'Детальніше',
         'button_url' => '#',
-        'highlighted' => true,
+        'highlighted' => false,
         'layout' => 'default',
     ],
     [
