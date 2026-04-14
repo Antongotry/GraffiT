458d9a969aa226eedf218ed264dbf2aca718c37d
mac <mac@MacBook-Pro-mac.local>
2026-04-14 23:00:50 +0300

    Update home showcase cover image

--- template-parts/sections/home-showcase.php
+++ template-parts/sections/home-showcase.php
@@ -7,7 +7,7 @@
 
 declare(strict_types=1);
 
-$home_showcase_image = 'https://lavenderblush-bat-855084.hostingersite.com/wp-content/uploads/2026/04/Group-1707480430_result-scaled.webp?v=home-showcase-2';
+$home_showcase_image = 'https://lavenderblush-bat-855084.hostingersite.com/wp-content/uploads/2026/04/Group-1707480431_result-scaled.webp?v=home-showcase-3';
 ?>
 <section class="home-showcase js-home-showcase" aria-label="<?php esc_attr_e('Вітрина GraffiT', 'graffit'); ?>">
     <div class="home-showcase__media js-home-showcase-media" aria-hidden="true">
