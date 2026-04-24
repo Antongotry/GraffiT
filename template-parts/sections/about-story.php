f7519c7ecd43be28116895627b19e7fb65d376cc
mac <mac@MacBook-Pro-mac.local>
2026-04-24 15:53:14 +0300

    Move about seam to story background

--- template-parts/sections/about-story.php
+++ template-parts/sections/about-story.php
@@ -6,8 +6,10 @@
  */
 
 declare(strict_types=1);
+
+$about_story_image_url = graffit_about_story_image_url();
 ?>
-<section class="about-story" aria-labelledby="about-story-title">
+<section class="about-story" aria-labelledby="about-story-title" style="--about-story-seam-image: url('<?php echo esc_url($about_story_image_url); ?>');">
     <div class="about-story__backdrop" aria-hidden="true"></div>
 
     <div class="about-story__container">
@@ -40,7 +42,7 @@ declare(strict_types=1);
         <div class="about-story__media" aria-hidden="true">
             <img
                 class="about-story__image"
-                src="<?php echo esc_url(graffit_about_story_image_url()); ?>"
+                src="<?php echo esc_url($about_story_image_url); ?>"
                 width="810"
                 height="951"
                 alt=""
