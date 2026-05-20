706203f20287f072d8be552c8b6569a67033f916
mac <mac@MacBook-Pro-mac.local>
2026-05-20 18:56:40 +0300

    Restore en dash in GraffIT about lead lines

--- template-parts/sections/product-mediahub-about.php
+++ template-parts/sections/product-mediahub-about.php
@@ -56,7 +56,7 @@ $mediahub_about_trust_cards = [
                     </div>
 
                     <h2 class="services-clients__title" id="mediahub-about-title">
-                        <span class="services-clients__title-line"><strong>GraffIT</strong> - команда, що вийшла з </span>
+                        <span class="services-clients__title-line"><strong>GraffIT</strong> – команда, що вийшла з </span>
                         <span class="services-clients__title-line">великого внутрішнього ІТ-департаменту, </span>
                         <span class="services-clients__title-line">який десятиліттями будував </span>
                         <span class="services-clients__title-line">інфраструктуру для торговельних </span>
