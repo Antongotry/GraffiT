f1fc0dad787e12a8c459014ed0ced47ddf0f30ef
mac <mac@MacBook-Pro-mac.local>
2026-05-20 14:49:54 +0300

    Animate low-copy service card content up on hover and restore low layout

--- template-parts/sections/services-overview.php
+++ template-parts/sections/services-overview.php
@@ -22,7 +22,7 @@ $cards = [
         'button_label' => 'Детальніше',
         'button_url' => '#',
         'highlighted' => false,
-        'layout' => 'default',
+        'layout' => 'low',
     ],
     [
         'title' => 'IT-аутсорсинг',
